276bdcc843cf1a3cd81d22df7ce2c12db11f3d14
jwl <james.warren.leung@pm.me>
2020-12-06 00:56:55 -0800

    Cleaned up commented out code.

--- day06-01/day06-01.go
+++ day06-01/day06-01.go
@@ -29,33 +29,21 @@ func splitByNewlines(rawString string) []string {
 func getAllUniqueLetters(group []string) string {
 	uniqueLetters := ""
 	for _, individual := range group {
-		// fmt.Printf("individual is: %s\n", individual)
 		for _, c := range individual {
-			// fmt.Printf("c is: %s\n", string(c))
 			if strings.Count(uniqueLetters, string(c)) == 0 {
 				uniqueLetters = uniqueLetters + string(c)
 			}
 		}
 	}
-	// fmt.Printf("for group %#v, uniqueLetters are: %s\n", group, uniqueLetters)
 	return uniqueLetters
 }
 
 func main() {
 	fmt.Println("day06-01 started")
-	// boardingPasses := loadInput("input")
-	// fmt.Printf("Missing seatID is: %d\n", getMissingSeatID(boardingPasses))
-
 	groupList := splitByBlanklines(loadInput("input"))
-
 	sum := 0
 
 	for _, group := range groupList {
-		// fmt.Printf("group is:\n%s\n", group)
-		// for _, individual := range splitByNewlines(group) {
-		// 	fmt.Printf("\tindividual in group is: %s\n", individual)
-		// }
-		// fmt.Printf("uniqueLetters of this group are: %s\n\n", getAllUniqueLetters(splitByNewlines(group)))
 		sum += len(getAllUniqueLetters(splitByNewlines(group)))
 	}
 
